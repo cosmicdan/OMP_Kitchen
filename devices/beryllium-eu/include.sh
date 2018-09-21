a3700249682b7cb9133ead095f26f7d76dfeb224
CosmicDan <dconnolly.au@gmail.com>
2018-09-21 13:56:45 +1000

    beryllium-eu does not need Magisk (verity already disabled)

--- devices/beryllium-eu/include.sh
+++ devices/beryllium-eu/include.sh
@@ -12,8 +12,7 @@ deviceDoRoots() {
 updaterScriptFooter() {
 	echo "ui_print(\" \");"
 	echo "ui_print(\"All done! Remember:\");"
-	echo "ui_print(\"1) Flash Magisk to avoid fastboot-kick!\");"
-	echo "ui_print(\"2) Flash FBE-Disable ZIP to REMOVE\");"
+	echo "ui_print(\" - Flash FBE-Disable ZIP to REMOVE\");"
 	echo "ui_print(\"   file-based encryption (optional)\");"
 	echo "ui_print(\" \");"
 }
