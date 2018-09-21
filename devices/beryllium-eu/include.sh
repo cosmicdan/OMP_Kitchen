f8c3d353fb482023c2ad9e1638cbb6f0210330c8
CosmicDan <dconnolly.au@gmail.com>
2018-09-21 14:39:25 +1000

    Re-enable FBE for beryllium-eu (works fine now)

--- devices/beryllium-eu/include.sh
+++ devices/beryllium-eu/include.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 deviceDoRoots() {
-	echo "    [i] FBE temporarily disabled (causes userdata permission issues on xiaomi.eu ROM's; still investigating cause)"
-	# Disabled for now (needs debugging)
-	#echo "    [#] Modify vendor for file-based encryption (compatible TWRP required) ..."
-	#verifyFilesExist "${miuiOutPath}/vendor/etc/fstab.qcom"
-	#sed -i -e '/userdata/ s/forceencrypt=footer/fileencryption=ice/' "${miuiOutPath}/vendor/etc/fstab.qcom"
-	#sed -i -e '/userdata/ s/encryptable=footer/fileencryption=ice/' "${miuiOutPath}/vendor/etc/fstab.qcom"
+	echo "    [#] Modify vendor for file-based encryption (a compatible TWRP is required) ..."
+	verifyFilesExist "${miuiOutPath}/vendor/etc/fstab.qcom"
+	sed -i -e '/userdata/ s/forceencrypt=footer/fileencryption=ice/' "${miuiOutPath}/vendor/etc/fstab.qcom"
+	sed -i -e '/userdata/ s/encryptable=footer/fileencryption=ice/' "${miuiOutPath}/vendor/etc/fstab.qcom"
 }
 
 updaterScriptFooter() {
